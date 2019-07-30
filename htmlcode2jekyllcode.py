36fb93af3ba4c605a76b17d63caa84a6545a6844
Sylvain Gendrot <sylvain.gendrot@gmail.com>
2019-07-30 22:26:25 +0200

    cleaner of useless tags done. Beginning of the img cleaner

--- htmlcode2jekyllcode.py
+++ htmlcode2jekyllcode.py
@@ -26,7 +26,7 @@ def analyze_jekyll_file(name):
     :return: xxxx
     '''
     jekyll_file = files_dir+"/"+name
-    logger.info("Let process: %s" % jekyll_file)
+    logger.info("Let analyze: %s" % jekyll_file)
 
     file_data = open(jekyll_file, "r").read()
 
@@ -55,9 +55,59 @@ def process_jekyll_file(name):
 
     file_data = open(jekyll_file, "r").read()
 
-    file_data_clean = re.sub("<(/)?(p|di|center|strong|span)[^>]*>","", file_data)
-    logger.debug("data cleaned: %s"% file_data_clean)
+    # clean the useless tags
+    file_data_clean = re.sub("<(/)?(p|div|center|span|style)[^>]*>","", file_data)
+    # logger.debug("data : %s"% file_data)
+    # logger.debug("data cleaned: %s"% file_data_clean)
+    file_data_clean = re.sub("<(/)?(em)[^>]*>","*", file_data_clean)
+    file_data_clean = re.sub("<(/)?(strong)[^>]*>","**", file_data_clean)
 
+
+    jekyll_file_stream = open(jekyll_file, "w")
+    jekyll_file_stream.write(file_data_clean)
+    jekyll_file_stream.close()
+
+
+def clean_img_jekyll_file(name):
+    '''
+    clean and update the img tag (correct link and change tag )
+
+    :param name: the name of the file
+    :type name: basestring
+    :return: xxxx
+    '''
+    jekyll_file = files_dir+"/"+name
+    logger.info("Let process: %s" % jekyll_file)
+
+    file_data = open(jekyll_file, "r").read()
+    # logger.debug("data : %s"% file_data)
+
+    # capture all img tag to process them
+    all_img_tags = re.findall("<img([^>]*)/>", file_data)
+    for img_tag in all_img_tags:
+        # a dict of the param of the img tag
+        img_info = {}
+
+        #if the tag isn't present, I will user this empty array
+        nonexistent_tag = ['','']
+        logger.debug("a img tag -----> %s" % img_tag)
+        # not the most effective but more human readable
+        img_info['alt'] = (re.search("alt=\"([^\"]*)\"", img_tag) or nonexistent_tag)[1]
+        img_info['title'] = (re.search("title=\"([^\"]*)\"", img_tag) or nonexistent_tag)[1]
+        img_info['src'] = (re.search("src=\"([^\"]*)\"", img_tag)or nonexistent_tag)[1]
+        img_info['height'] = (re.search("height=\"([^\"]*)\"", img_tag) or nonexistent_tag)[1]
+        img_info['width'] = (re.search("width=\"([^\"]*)\"", img_tag)or nonexistent_tag)[1]
+        for key in img_info:
+            logger.debug("%s = %s"% (key, img_info[key]))
+
+
+    # replace old img tag by the new one
+    # file_data_clean = re.sub("<img([^>])*/>","", file_data)
+
+
+    # jekyll_file_stream = open(jekyll_file, "w")
+    # jekyll_file_stream.write(file_data_clean)
+    # jekyll_file_stream.close()
 
 
 ###########    MAIN    ###########
@@ -66,8 +116,9 @@ if __name__ == "__main__":
     # the dir contains only jekyll-post files
     for afile in os.listdir(files_dir):
         logger.debug ("call process_jekyll_file for %s" % afile)
-        analyze_jekyll_file(afile)
         # process_jekyll_file(afile)
+        # analyze_jekyll_file(afile)
+        clean_img_jekyll_file(afile)
     logger.info(tag_dic)
 
 
